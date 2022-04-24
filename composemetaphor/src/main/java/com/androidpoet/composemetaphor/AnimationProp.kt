c3d42831eb413713e2d42bd2cb0decccc3c28421
AndroidPoet <poetdroid2@gmail.com>
2022-04-24 19:17:26 +0530

    bugs fixes

--- composemetaphor/src/main/java/com/androidpoet/composemetaphor/AnimationProp.kt
+++ composemetaphor/src/main/java/com/androidpoet/composemetaphor/AnimationProp.kt
@@ -17,10 +17,9 @@
 package com.androidpoet.composemetaphor
 
 /*[AnimationProp] is properties of animation as compose has experimental apis we can change the internal code when needed.*/
-public data class AnimationProp(
+public data class AnimationProperties(
     val enterDuration: Int,
     val exitDuration: Int,
     val delay: Int,
-    val metaphorEasing: MetaphorEasing,
-    val initialAlpha: Float = 0.92f
+    val metaphorEasing: MetaphorEasing
 )
